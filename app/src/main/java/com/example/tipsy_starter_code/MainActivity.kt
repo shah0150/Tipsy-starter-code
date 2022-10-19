67952bbe3f148f555084db9178e0252da65d642c
Adesh <shaha@algonquincollege.com>
2022-10-19 19:40:33 -0400

    Tipsy Starter Code for MAD9112

---
+++ app/src/main/java/com/example/tipsy_starter_code/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.tipsy_starter_code
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.EditText
+import android.widget.SeekBar
+import android.widget.TextView
+
+private const val TAG = "MainActivity"
+private const val INITIAL_TIP_PERCENT = 15
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var tipsyBaseAmount: EditText
+    private lateinit var seekTipsy: SeekBar
+    private lateinit var tipsyPercentage: TextView
+    private lateinit var tipsyTipAmount: TextView
+    private lateinit var totalTipsyAmount: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        tipsyBaseAmount = findViewById(R.id.etBase)
+        seekTipsy = findViewById(R.id.seekTipsy)
+        tipsyPercentage = findViewById(R.id.tipsyPercentage)
+        tipsyTipAmount = findViewById(R.id.tipsyTipAmount)
+        totalTipsyAmount = findViewById(R.id.totalTipsyAmount)
+
+        seekTipsy.progress = INITIAL_TIP_PERCENT
+        tipsyPercentage.text = "$INITIAL_TIP_PERCENT%"
+        seekTipsy.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(SeekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                Log.i(TAG, "onProgressChanged $progress")
+                tipsyPercentage.text = "$progress%"
+
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+        })
+        tipsyBaseAmount.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun afterTextChanged(p0: Editable?) {
+                Log.i(TAG, "afterTextChanged $p0")
+
+
+            }
+        })
+    }
+
+}
